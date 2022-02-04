0e501ca3e75a2566678b8e6006640b6b5712c369
Vitor Jeronimo <vitor.bently@hotmail.com>
2022-02-04 15:11:05 -0400

    Switched layer buttons and ctrl

--- qmk_kbds/qmk_firmware/keyboards/lily58/keymaps/VitorJeronimo/keymap.c
+++ qmk_kbds/qmk_firmware/keyboards/lily58/keymaps/VitorJeronimo/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                     KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,  KC_MINS, \
   KC_BSPC,  KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                     KC_H,    KC_N,    KC_E,    KC_I,    KC_O,     KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LALT,  KC_RALT,  KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                        KC_LGUI, KC_LCTL, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_RCTL, KC_RGUI \
+                        KC_LGUI, MO(_LOWER), KC_LCTL, KC_SPC, KC_ENT, KC_RCTL, MO(_RAISE), KC_RGUI \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
