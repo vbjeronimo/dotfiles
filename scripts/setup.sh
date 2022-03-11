89a715be215dcab0773b502b554a7987df8f2f8f
Vitor Jeronimo <vitor.bently@hotmail.com>
2022-03-10 22:14:38 -0400

    Update setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -19,6 +19,14 @@ hostnamectl set-hostname $HOSTNAME
 pacman -S --noconfirm networkmanager
 systemctl enable NetworkManager
 
+# Basic firewall setup
+pacman -S --noconfirm ufw
+systemctl enable ufw
+ufw default deny
+ufw allow from 192.168.0.0/24
+ufw limit ssh
+ufw enable
+
 # Root management
 echo "root:$ROOT_PASSWORD" | chpasswd
 
@@ -27,8 +35,15 @@ pacman -S --noconfirm grub efibootmgr amd-ucode
 grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# User management
+# Install and config sudo
 pacman -S --noconfirm sudo
 sed -i 's/^# %wheel ALL=(ALL) ALL$/%wheel ALL=(ALL) ALL/' /etc/sudoers
+
+# User management
 useradd -m -G wheel $USERNAME
 echo "$USERNAME:$USER_PASSWORD" | chpasswd
+
+# install yay and the rest of the packages
+
+
+
