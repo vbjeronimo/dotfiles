e7992573808164ee61f2bfcfd8ad5c419dac65ac
Vitor Jeronimo <vitor.bently@hotmail.com>
2022-04-05 19:44:00 -0300

    Add setup_xrandr.sh script

---
+++ scripts/setup_xrandr.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+xrandr --output DP-2 --mode 3440x1440 --pos 0x0 --rotate normal --rate 100.00 --primary
+xrandr --output HDMI-0 --mode 1920x1080 --pos 760x1440 --rotate normal --rate 75.00
